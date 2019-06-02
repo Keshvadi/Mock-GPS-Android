31941c10dd7ed629bd22301ccee29920db9ee673
Babak <babakbushehr@gmail.com>
2019-06-02 21:31:56 +0430

    start

---
+++ app/src/main/java/ir/babak/mockgps/MainActivity.java
@@ -0,0 +1,17 @@
+package ir.babak.mockgps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.objectbox.BoxStore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //https://docs.objectbox.io/getting-started
+    }
+}
